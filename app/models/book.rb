7ff679e4b6c56d7ac8baddfa3c6df96ebb6a5ae3
EamonBachari <eamonbachari@gmail.com>
2017-07-26 16:48:13 -0500

    book model added.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   validates :title, presence: true,
                     length: { minimum: 5 }
-            :author, presence: true,
-            :description, presence: true,
-            :url, presence: true
+  validates :author, presence: true
+  validates :description, presence: true
+  validates :url, presence: true
 end
