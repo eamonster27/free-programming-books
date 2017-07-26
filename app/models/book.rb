1a5f99ac5aadc985f62af54b191952cd4c07547f
EamonBachari <eamonbachari@gmail.com>
2017-07-26 16:59:09 -0500

    Messing with models.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,4 @@
 class Book < ApplicationRecord
   validates :title, presence: true,
                     length: { minimum: 5 }
-  validates :author, presence: true
-  validates :description, presence: true
-  validates :url, presence: true
 end
